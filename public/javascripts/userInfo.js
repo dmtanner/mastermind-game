04c60abdcb245069fe063016510616af1c9667df
drewfus <ec2-user@ip-172-31-29-42.us-west-2.compute.internal>
2015-04-02 23:30:10 +0000

    added functionality to signup

--- public/javascripts/userInfo.js
+++ public/javascripts/userInfo.js
@@ -13,4 +13,22 @@ angular.module('userInfo', [])
       return $http.post('/login', UserInfo);
     };
   }
-]);
+])
+.controller('SignupCtrl', [
+  '$scope',
+  '$http',
+  function($scope, $http) {
+    $scope.sendUserInfo = function() {
+      if ($scope.userName === '') {return;}
+      if ($scope.passWord === '') {return;}
+      if ($scope.email === '') {return;}
+      var UserInfo = {
+        username: $scope.userName,
+        password: $scope.passWord,
+	email: $scope.email
+      }
+      return $http.post('/signup', UserInfo);
+    };
+  }
+])
+
