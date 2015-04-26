771c0f23b2c53a8a83d8952dd0f14f59e418b121
dmtanner <kritiosboy@gmail.com>
2015-04-25 19:24:55 -0600

    Create highscore.js

---
+++ models/highscore.js
@@ -0,0 +1,7 @@
+var mongoose = require('mongoose'),
+	Schema = mongoose.Schema;
+var HighscoreSchema = new Schema({
+		username: String,
+  	score: Number
+});
+mongoose.model("Highscore", HighscoreSchema);
